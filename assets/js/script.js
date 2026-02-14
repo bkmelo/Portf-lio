ea9c95f44b9556d85079c8b3f1e4056d250ed2af
Bruna Melo <bmelo.silva25@gmail.com>
2026-02-14 20:55:12 -0300

    curriculo

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,8 @@ async function getAboutGithub() {
         <div class="about-buttons-data">
           <div class="buttons-container">
             <a href="${perfil.html_url}" target="_blank" class="botao">Ver GitHub</a>
-            <a href="#" target="_blank" class="botao-outline">Currículo</a>
+
+            <a href="./assets/docs/CURRICULO.2026.1.pdf" target="_blank" class="botao-outline">Currículo</a>
           </div>
 
           <div class="data-container">
